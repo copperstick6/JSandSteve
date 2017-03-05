e8839463fb84202d399c68e13b93197e1d8eb538
William Han <han818william@hotmail.com>
2017-03-05 15:16:08 -0600

    updated, fixes deployed

--- app.py
+++ app.py
@@ -2,27 +2,20 @@ from flask import Flask, request, render_template, url_for
 import webScraper
 
 app = Flask(__name__)
+
 @app.route('/')
 def getHomepage():
     return render_template('homepage.html')
 
 @app.route('/', methods = ['POST'])
-def process(): 
-    text = request.form['text']
-	canScrape = webScraper.findJS(text)
-	if(canScrape == "error, invalid url")
-	{
-		return render_template('invalid_url.html')
-	}
-	else if(canScrape == "weird charSet")
-	{
-		return render_template('charSet.html')
-	}
-	else if(canScrape == "it has js")
-	{
-		return render_template('hasJS.html')
-	}
-	else
-	{
-		return render_template('noJS.html')
-	}
+def process():
+  text = request.form['text']
+  canScrape = webScraper.findJS(text)
+  if(canScrape == "error, invalid url"):
+    return render_template('invalid_url.html')
+  if(canScrape == "weird charSet"):
+    return render_template('charSet.html')
+  if(canScrape == "it has js"):
+    return render_template('hasJS.html')
+  else:
+    return render_template('noJS.html')
