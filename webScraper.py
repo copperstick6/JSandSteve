e4a086d2f52d806804266916c644ca3349c67011
William Han <han818william@hotmail.com>
2017-03-05 15:16:46 -0600

    editing tabbing

--- webScraper.py
+++ webScraper.py
@@ -3,14 +3,14 @@ import codecs
 
 def findJS(url):
     url = str(url)
-	try:
-		valid = urllib.request.urlopen(url)
-	except:
-		return "error, invalid url"
+    try:
+        valid = urllib.request.urlopen(url)
+    except:
+        return "error, invalid url"
     try:
         temp = urllib.request.urlopen(url).read().decode("utf-8")
         if("<script>" in temp):
             return "it has js"
-		return "no js"
+        return "no js"
     except:
         return "weird charSet"
